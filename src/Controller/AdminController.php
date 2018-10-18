4af0376245ed5f9ee2377e2028d48c87920704e3
Raphaël <raphael.amorim.goncalves@gmail.com>
2018-10-18 10:47:26 +0200

    new commit pour admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\EditType;
 use App\Form\TestType;
+use App\Entity\CommentArticle;
 use App\Form\RegistrationType;
 use App\Form\ChangePasswordType;
 use App\Controller\AdminController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,10 +31,41 @@ class AdminController extends AbstractController
      * @Route("/admin/show_user", name="show_user")
      */
     public function showUser(){
-        return $this->render('admin/show_user.html.twig');
+
+        $repo = $this->getDoctrine()->getRepository(User::class);
+
+        $users = $repo->findAll();
+
+        return $this->render('admin/show_user.html.twig',[
+            'controller_name' => 'AdminController',
+            'users' => $users
+        ]);
     }
 
+    /**
+     * @Route("/admin/delete/user/{id}" , name="delete_user")
+     */
+    public function deleteUser(User $user, EntityManagerInterface $em) {
+        $em->remove($user);
+        $em->flush();
 
+        return $this->redirectToRoute('show_user');
+
+    }
+
+    /**
+     * @Route("admin/show_comment_article", name="show_user")
+     */
+    public function showCommentArticle(){
+        $repo = $this->getDoctrine()->getRepository(CommentArticle::class);
+
+        $users = $repo->findAll();
+
+        return $this->render('admin/show_comment_article.html.twig',[
+            'controller_name' => 'AdminController',
+            'users' => $users
+        ]);
+    }
 
 
 
