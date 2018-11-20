1d9828b1d10eb090cbee5f8bd82c08909c26ea59
Luc Leveque <luc.leveque78@gmail.com>
2018-11-20 12:38:12 +0100

    Flux Actualités

---
+++ src/Controller/ActualiteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActualiteController extends AbstractController
+{
+    /**
+     * @Route("/actualite", name="actualite")
+     */
+    public function index()
+    {
+        $curl = curl_init();
+        curl_setopt($curl,CURLOPT_URL,"http://plus.lefigaro.fr/tag/apiculture/rss.xml");
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+        $contenu = curl_exec($curl);
+        $xml = new \SimpleXMLElement($contenu);
+
+        dump($xml);
+
+
+        return $this->render('actualite/index.html.twig', [
+            'controller_name' => 'ActualiteController',
+            'articles' =>$xml
+        ]);
+        }
+
+}
